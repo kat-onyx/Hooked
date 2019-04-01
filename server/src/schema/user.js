642b5ca4586a042e8c93cee2d4221437814f68d4
kat <sarah.kathryn.peters@gmail.com>
2019-04-01 16:26:32 -0700

    Add mutation for SignUp in User Schema

--- server/src/schema/user.js
+++ server/src/schema/user.js
@@ -13,6 +13,19 @@ export default gql`
         me: User
         user(id: ID!): User
     }
+
+    extend type Mutation {
+        signUp(
+            username: String!
+            email: String!
+            password: String!
+        ): Token!
+    }
+
+    type Token {
+        token: String!
+    }
+    
     type User {
         id: ID!
         username: String!
